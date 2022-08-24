6af34fe531a86c899634531460b167559e22e487
yuriykolomiyets <juriy121@gmail.com>
2022-08-24 14:35:02 +0200

    67. Create Owners Page &  Controller

--- pet-clinic-web/src/main/java/guru/springframework/sfgdi/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgdi/controllers/VetController.java
@@ -1,7 +1,8 @@
 package guru.springframework.sfgdi.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
 public class VetController {
 
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
