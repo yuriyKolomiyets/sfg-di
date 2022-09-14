f2bd2b7e397a1a176fd56a2739b9a0867ca121f9
yuriykolomiyets <juriy121@gmail.com>
2022-09-14 19:14:00 +0200

    241. Spring Pet Clinic - Visit Controller Tests

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/VisitControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/VisitControllerTest.java
@@ -96,7 +96,8 @@ class VisitControllerTest {
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                         .param("date","2018-11-11")
                         .param("description", YET_ANOTHER_VISIT_DESCRIPTION))
-                .andExpect(status().isOk())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name(REDIRECT_OWNERS_1))
                 .andExpect(model().attributeExists("visit"))
         ;
     }
